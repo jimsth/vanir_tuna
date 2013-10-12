cdcd1bde78e0bafed49dd8bb281291dc773beed9
Kostas K <mpokwsths@gmail.com>
2013-10-12 16:25:50 +0300

    Enable SCU Speculative linefills
    By enabling SCU Speculative linefills in the SCU Control
    Register, the  L2 cache access latency seems tp be reduced
    a bit with an overall ~1.5% improvement.
    
    Signed-off-by: Dan Trifan <jimsth@gmail.com>

--- arch/arm/kernel/smp_scu.c
+++ arch/arm/kernel/smp_scu.c
@@ -47,10 +47,8 @@ void __init scu_enable(void __iomem *scu_base)
 #endif
 
 	scu_ctrl = __raw_readl(scu_base + SCU_CTRL);
-	/* already enabled? */
-	if (scu_ctrl & 1)
-		return;
 
+	scu_ctrl |= (1 << 3);
 	scu_ctrl |= 1;
 	__raw_writel(scu_ctrl, scu_base + SCU_CTRL);
 

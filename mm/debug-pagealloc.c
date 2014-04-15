38ee79076878e83fd4f6ff3b6ac315dc49102200
Laura Abbott <lauraa@codeaurora.org>
2014-04-14 20:09:44 -0700

    mm: Mark free pages as read only
    Drivers have a tendency to scribble on everything, including free
    pages. Make life easier by marking free pages as read only when
    on the buddy list and re-marking as read/write when allocating.
    
    Change-Id: I978ed2921394919917307b9c99217fdc22f82c59
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>
    Signed-off-by: Dan Trifan <jimsth@gmail.com>

--- mm/debug-pagealloc.c
+++ mm/debug-pagealloc.c
@@ -4,6 +4,14 @@
 #include <linux/page-debug-flags.h>
 #include <linux/poison.h>
 
+#ifndef mark_addr_rdonly
+#define mark_addr_rdonly(a)
+#endif
+
+#ifndef mark_addr_rdwrite
+#define mark_addr_rdwrite(a)
+#endif
+
 static inline void set_page_poison(struct page *page)
 {
 	__set_bit(PAGE_DEBUG_FLAG_POISON, &page->debug_flags);
@@ -25,6 +33,7 @@ static void poison_page(struct page *page)
 
 	set_page_poison(page);
 	memset(addr, PAGE_POISON, PAGE_SIZE);
+	mark_addr_rdonly(addr);
 	kunmap_atomic(addr);
 }
 
@@ -81,6 +90,7 @@ static void unpoison_page(struct page *page)
 
 	addr = kmap_atomic(page);
 	check_poison_mem(addr, PAGE_SIZE);
+	mark_addr_rdwrite(addr);
 	clear_page_poison(page);
 	kunmap_atomic(addr);
 }
